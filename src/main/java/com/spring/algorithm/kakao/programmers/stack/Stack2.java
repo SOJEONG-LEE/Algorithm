7a005f59ffb698e95975c6fdae4cb9a83ada8ea1
sojeong <bear064@naver.com>
2022-01-10 23:58:05 +0900

    2022/01/10 프로그래머스 > 스택/힙 > 프린터

--- src/main/java/com/spring/algorithm/kakao/programmers/stack/Stack2.java
+++ src/main/java/com/spring/algorithm/kakao/programmers/stack/Stack2.java
@@ -4,6 +4,7 @@ import com.spring.algorithm.common.AbstractAlgorithm;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 @Service("stack2")
 public class Stack2 extends AbstractAlgorithm {
@@ -16,7 +17,7 @@ public class Stack2 extends AbstractAlgorithm {
         this.location = 0;
     }
 
-    public int test1() {
+    public void test1() {
         int answer = 0;
         int max;
         int i;
@@ -31,10 +32,10 @@ public class Stack2 extends AbstractAlgorithm {
             }
             if (i == location) break;
         }
-        return answer;
+        System.out.println(answer);
     }
 
-    public int solution1() {
+    public void solution1() {
         int answer = 0;
         int l = location;
 
@@ -61,10 +62,10 @@ public class Stack2 extends AbstractAlgorithm {
             }
         }
 
-        return answer;
+        System.out.println(answer);
     }
 
-    public int solution2() {
+    public void solution2() {
         List<Integer> list = new ArrayList<>();
         for (int priority : priorities) {
             list.add(priority);
@@ -77,7 +78,7 @@ public class Stack2 extends AbstractAlgorithm {
                 list.add(list.remove(0));
             } else {
                 if (location == 0) {
-                    return turn;
+                    System.out.println(turn);
                 }
                 list.remove(0);
                 turn++;
@@ -89,13 +90,34 @@ public class Stack2 extends AbstractAlgorithm {
                 location = list.size() - 1;
             }
         }
-        return 0;
+        System.out.println(0);
+    }
+
+    public void test2(){
+        int answer = 0;
+        int index = location;
+        List<Integer> queue = Arrays.stream(priorities).boxed().collect(Collectors.toList());
+        List<Integer> print = Arrays.stream(priorities).boxed()
+                .sorted(Collections.reverseOrder()).collect(Collectors.toList());
+
+        while(!queue.isEmpty()){
+            int num = queue.remove(0);
+            if(num==print.get(0)){
+                print.remove(0);
+                answer++;
+                if(index==0) System.out.println(answer);
+            }else{
+                queue.add(num);
+            }
+            index--;
+            if (index == -1) index = print.size()-1;
+        }
+
+        System.out.println(answer);
     }
 
     @Override
     public <T> void printResult(T object) {
-        T answer = object;
-        System.out.println(answer);
 
     }
 
