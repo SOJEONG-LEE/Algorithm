424251be937db6de309634d298ddf4fdb5b89721
sojeong <bear064@naver.com>
2022-01-18 23:54:32 +0900

    2022/01/18 프로그래머스 > 연습문제 > 두 정수 사이의 합

---
+++ src/main/java/com/spring/algorithm/kakao/programmers/practice/두정수사이의합.java
@@ -0,0 +1,42 @@
+package com.spring.algorithm.kakao.programmers.practice;
+
+import com.spring.algorithm.common.AbstractAlgorithm;
+import org.springframework.stereotype.Service;
+
+@Service("두정수사이의합")
+public class 두정수사이의합 extends AbstractAlgorithm {
+
+    private int a;
+    private int b;
+
+    public 두정수사이의합() {
+        this.a = 3;
+        this.b = 5;
+    }
+
+    @Override
+    public <T> void printResult(T answer) {
+
+    }
+
+    public void test1() {
+        int min=0;
+        int max=0;
+        if(a == b) System.out.println(a);
+        else if(a > b){
+            max = a;
+            min = b;
+        }else{
+            max = b;
+            min =a;
+        }
+        int answer = 0;
+        for(int i=min; i<=max; i++){
+            answer += i;
+        }
+        System.out.println(answer);
+
+    }
+
+
+}
