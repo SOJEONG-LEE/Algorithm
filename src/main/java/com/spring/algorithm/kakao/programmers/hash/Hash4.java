f158c3bb88c062682a56d23c902eb5b0c346bba4
sojeong <bear064@naver.com>
2022-01-23 23:18:19 +0900

    2022/01/23 프로그래머스 > 해시 > 베스트앨범

--- src/main/java/com/spring/algorithm/kakao/programmers/hash/Hash4.java
+++ src/main/java/com/spring/algorithm/kakao/programmers/hash/Hash4.java
@@ -1,22 +1,55 @@
 package com.spring.algorithm.kakao.programmers.hash;
 
-import java.util.HashMap;
-import java.util.Map;
+import com.spring.algorithm.common.AbstractAlgorithm;
+import org.springframework.stereotype.Service;
 
-public class Hash4 {
+import java.util.*;
 
-    public void test1(){
-        String[] genres ={"classic", "pop", "classic", "classic", "pop"};
-        int[] plays = {500, 600, 150, 800, 2500};
+@Service("hash4")
+public class Hash4 extends AbstractAlgorithm {
+
+    private String[] genres;
+    private int[] plays;
+
+    public Hash4() {
+        this.genres = new String[]{"classic", "pop", "classic", "classic", "pop"};
+        this.plays = new int[]{500, 600, 150, 800, 2500};
+    }
+
+    public void test1() {
+        List<Integer> answer = new ArrayList<>();
+        PriorityQueue<Map<String, Integer>> genreQueue = new PriorityQueue<>((a, b) -> Integer.compare(b.values().stream().findFirst().get(), a.values().stream().findFirst().get()));
+        Map<String, Integer> sumQueue = new HashMap<>();
+        Map<String, PriorityQueue<int[]>> queueMap = new HashMap<>();
+
+        for (int i = 0; i < genres.length; i++) {
+            if (!queueMap.containsKey(genres[i])) queueMap.put(genres[i], new PriorityQueue<int[]>((a, b) -> Integer.compare(b[1], a[1])));
+            queueMap.get(genres[i]).add(new int[]{i, plays[i]});
+            sumQueue.put(genres[i], sumQueue.getOrDefault(genres[i], 0) + plays[i]);
+        }
+
+        for(String genre : sumQueue.keySet()){
+            genreQueue.add(new HashMap<String,Integer>(){{put(genre, sumQueue.get(genre));}});
+        }
+
+        while (!genreQueue.isEmpty()){
+            String genre = genreQueue.poll().keySet().stream().findFirst().get();
+            answer.add(queueMap.get(genre).poll()[0]);
+            if(! queueMap.get(genre).isEmpty()) answer.add(queueMap.get(genre).poll()[0]);
+        }
+        System.out.println(answer);
+    }
+
+    public void test2() {
         int[] anwser;
 //        Map<String, > playMapping = new HashMap<>();
         Map<String, Map<String, Integer>> genresSum = new HashMap<>();
 
-        for(int i=0; i< genres.length; i++){
+        for (int i = 0; i < genres.length; i++) {
             String genresKey = genres[i];
-            if(genresSum.containsKey(genresKey)) {
-                genresSum.get(genresKey).put("sum", genresSum.get(genresKey).get("sum")+plays[i]);
-            }else{
+            if (genresSum.containsKey(genresKey)) {
+                genresSum.get(genresKey).put("sum", genresSum.get(genresKey).get("sum") + plays[i]);
+            } else {
                 Map<String, Integer> playMapping = new HashMap<>();
                 playMapping.put("sum", plays[i]);
                 playMapping.put(String.valueOf(plays[i]), i);
@@ -24,6 +57,11 @@ public class Hash4 {
             }
         }
 
+
+    }
+
+    @Override
+    public <T> void printResult(T answer) {
 
     }
 }
