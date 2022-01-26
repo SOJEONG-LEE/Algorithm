fb2e92f4a68d2f59faff55c15a21cf7a5eb3da38
sojeong <bear064@naver.com>
2022-01-26 23:56:51 +0900

    2022/01/26 프로그래머스 > 월간 코드 챌린지 시즌3 > 없는 숫자 더하기

---
+++ src/main/java/com/spring/algorithm/kakao/programmers/practice/없는숫자더하기.java
@@ -0,0 +1,27 @@
+package com.spring.algorithm.kakao.programmers.practice;
+
+import com.spring.algorithm.common.AbstractAlgorithm;
+import org.springframework.stereotype.Service;
+
+@Service("없는숫자더하기")
+public class 없는숫자더하기 extends AbstractAlgorithm {
+
+    private int[] numbers;
+
+    public 없는숫자더하기() {
+        this.numbers = new int[]{1,2,3,4,6,7,8,0};
+    }
+
+    @Override
+    public <T> void printResult(T answer) {
+
+    }
+
+    public void test1() {
+        int answer = (0+9)*(9-0+1)/2;
+        for(int num : numbers){
+            answer -= num;
+        }
+        System.out.println(answer);
+    }
+}
