6d0c6b60896923cebbd0aea624a822abbd90e2ed
sojeong <bear064@naver.com>
2022-01-27 23:59:24 +0900

    2022/01/27 프로그래머스 > 월간 코드 챌린지 시즌3 > 문자열 다루기 기본

---
+++ src/main/java/com/spring/algorithm/kakao/programmers/practice/문자열다루기기본.java
@@ -0,0 +1,28 @@
+package com.spring.algorithm.kakao.programmers.practice;
+
+import com.spring.algorithm.common.AbstractAlgorithm;
+import org.springframework.stereotype.Service;
+
+@Service("문자열다루기기본")
+public class 문자열다루기기본 extends AbstractAlgorithm {
+
+    private String s;
+
+    public 문자열다루기기본() {
+        this.s = "1234";
+    }
+
+    @Override
+    public <T> void printResult(T answer) {
+
+    }
+
+    public void test1() {
+        boolean answer = Boolean.TRUE;
+        if(s.length() != 4 || s.length() !=6 )System.out.println(Boolean.FALSE);
+        for (int i = 0; i < s.length(); i++) {
+            if (!Character.isDigit(s.charAt(i))) System.out.println(Boolean.FALSE);
+        }
+        System.out.println(answer);
+    }
+}
