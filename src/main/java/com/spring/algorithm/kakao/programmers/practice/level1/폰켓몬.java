90da841f0295c1e43206a455d5be2d9accec61ca
sojeong <bear064@naver.com>
2022-03-12 23:30:11 +0900

    2022/03/12 프로그래머스 > 찾아라 프로그래밍 마에스터 > 폰켓몬

---
+++ src/main/java/com/spring/algorithm/kakao/programmers/practice/level1/폰켓몬.java
@@ -0,0 +1,32 @@
+package com.spring.algorithm.kakao.programmers.practice.level1;
+
+import com.spring.algorithm.common.AbstractAlgorithm;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Service("폰켓몬")
+public class 폰켓몬 extends AbstractAlgorithm {
+
+    private int[] nums;
+
+    public 폰켓몬() {
+        this.nums = new int[]{3, 3, 3, 2, 2, 2};
+    }
+
+    @Override
+    public <T> void printResult(T answer) {
+
+    }
+
+    public void test1() {
+        int answer = 0;
+        int maxNum = nums.length / 2;
+        Set<Integer> set = Arrays.stream(nums).boxed().collect(Collectors.toSet());
+        answer = set.size() < maxNum ? set.size() : maxNum;
+
+        System.out.println(answer);
+    }
+}
