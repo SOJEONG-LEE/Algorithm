9a8be378a1ca63a55f4a7e8a37df18d3c5ee7fa2
sojeong <bear064@naver.com>
2022-03-27 23:59:39 +0900

    2022/03/27 SW Expert Academy > Problem > 1545. 거꾸로 출력해 보아요

---
+++ src/main/java/com/spring/algorithm/sw/expert/academy/problem/거꾸로출력해보아요.java
@@ -0,0 +1,26 @@
+package com.spring.algorithm.sw.expert.academy.problem;
+
+import com.spring.algorithm.common.AbstractAlgorithm;
+import org.springframework.stereotype.Service;
+
+import java.util.Scanner;
+
+@Service("거꾸로출력해보아요")
+public class 거꾸로출력해보아요 extends AbstractAlgorithm {
+
+    public 거꾸로출력해보아요() {
+    }
+
+    @Override
+    public <T> void printResult(T answer) {
+
+    }
+
+    public void test1() {
+        Scanner sc = new Scanner(System.in);
+        int T;
+        T=sc.nextInt();
+    }
+
+
+}
