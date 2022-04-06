3f75fcf7e1d9e4a6e6ece09bfdc48bc626b7d01c
sojeong <bear064@naver.com>
2022-04-06 21:34:56 +0900

    2022/04/06 SW Expert Academy > Problem > 2063. 중간값 찾기

---
+++ src/main/java/com/spring/algorithm/sw/expert/academy/problem/중간값찾기.java
@@ -0,0 +1,41 @@
+package com.spring.algorithm.sw.expert.academy.problem;
+
+import com.spring.algorithm.common.AbstractAlgorithm;
+import org.springframework.stereotype.Service;
+
+import java.util.Scanner;
+
+@Service("평균값구하기")
+public class 평균값구하기 extends AbstractAlgorithm {
+
+    public 평균값구하기() {
+        /*
+3
+3 17 1 39 8 41 2 32 99 2
+22 8 5 123 7 2 63 7 3 46
+6 63 2 3 58 76 21 33 8 1
+         */
+    }
+
+    @Override
+    public <T> void printResult(T answer) {
+
+    }
+
+    public void test1() {
+        Scanner sc = new Scanner(System.in);
+        int T;
+        T = sc.nextInt();
+
+        for (int test_case = 1; test_case <= T; test_case++) {
+            int sum = 0;
+            for (int i = 1; i <= 10; i++) {
+                int num = sc.nextInt();
+                sum += num;
+            }
+            System.out.println("#" + test_case + " " + (int) Math.round(sum / 10d));
+        }
+    }
+
+
+}
