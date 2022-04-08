f5bcfb25770ac15861cb0485ddf79f1fcc1a15eb
sojeong <bear064@naver.com>
2022-04-08 22:55:10 +0900

    2022/04/08 SW Expert Academy > Problem > 1938. 아주 간단한 계산기

---
+++ src/main/java/com/spring/algorithm/sw/expert/academy/problem/아주간단한계산기.java
@@ -0,0 +1,35 @@
+package com.spring.algorithm.sw.expert.academy.problem;
+
+import com.spring.algorithm.common.AbstractAlgorithm;
+import org.springframework.stereotype.Service;
+
+import java.util.Scanner;
+
+@Service("아주간단한계산기")
+public class 아주간단한계산기 extends AbstractAlgorithm {
+
+    public 아주간단한계산기() {
+        /*
+8 3
+         */
+    }
+
+    @Override
+    public <T> void printResult(T answer) {
+
+    }
+
+    public void test1() {
+        Scanner sc = new Scanner(System.in);
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+
+        System.out.println(num1 + num2);
+        System.out.println(num1 - num2);
+        System.out.println(num1 * num2);
+        System.out.println(num1 / num2);
+
+    }
+
+
+}
