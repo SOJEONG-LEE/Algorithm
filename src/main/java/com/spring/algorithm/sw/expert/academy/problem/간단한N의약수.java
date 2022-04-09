82d5214a5dd593c975fb8fa6f3da90c8b8784458
sojeong <bear064@naver.com>
2022-04-09 23:59:10 +0900

    2022/04/09 SW Expert Academy > Problem > 1933. 간단한 N 의 약수

--- src/main/java/com/spring/algorithm/sw/expert/academy/problem/간단한N의약수.java
+++ src/main/java/com/spring/algorithm/sw/expert/academy/problem/간단한N의약수.java
@@ -5,15 +5,12 @@ import org.springframework.stereotype.Service;
 
 import java.util.Scanner;
 
-@Service("몫과나머지출력하기")
-public class 몫과나머지출력하기 extends AbstractAlgorithm {
+@Service("간단한N의약수")
+public class 간단한N의약수 extends AbstractAlgorithm {
 
-    public 몫과나머지출력하기() {
+    public 간단한N의약수() {
         /*
-3
-9 2
-15 6
-369 15
+10
          */
     }
 
@@ -24,16 +21,10 @@ public class 몫과나머지출력하기 extends AbstractAlgorithm {
 
     public void test1() {
         Scanner sc = new Scanner(System.in);
-        int T;
-        T=sc.nextInt();
+        int num = sc.nextInt();
 
-        for(int test_case = 1; test_case <= T; test_case++)
-        {
-            int num1 = sc.nextInt();
-            int num2 = sc.nextInt();
-            int x = num1 / num2;
-            int y = num1 % num2;
-            System.out.println("#"+test_case+" "+x+" "+y);
+        for (int i = 1; i <= num; i++) {
+            if (num % i == 0) System.out.print(i + " ");
         }
     }
 
