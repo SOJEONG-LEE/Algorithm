cf8e44c7476ae09139e53549535d7b93ee61396a
sojeong <bear064@naver.com>
2022-04-17 23:59:43 +0900

    2022/04/17 SW Expert Academy > Problem > 2025. N줄덧셈

---
+++ src/main/java/com/spring/algorithm/sw/expert/academy/problem/N줄덧셈.java
@@ -0,0 +1,29 @@
+package com.spring.algorithm.sw.expert.academy.problem;
+
+import com.spring.algorithm.common.AbstractAlgorithm;
+import org.springframework.stereotype.Service;
+
+import java.util.Scanner;
+
+@Service("N줄덧셈")
+public class N줄덧셈 extends AbstractAlgorithm {
+
+    public N줄덧셈() {
+        /*
+10
+         */
+    }
+
+    @Override
+    public <T> void printResult(T answer) {
+
+    }
+
+    public void test1() {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        System.out.println(num);
+    }
+
+
+}
--- src/main/java/com/spring/algorithm/sw/expert/academy/problem/일대일가위바위보.java
+++ src/main/java/com/spring/algorithm/sw/expert/academy/problem/일대일가위바위보.java
@@ -20,13 +20,23 @@ public class 일대일가위바위보 extends AbstractAlgorithm {
     }
 
     public void test1() {
+        /*
+        1: 가위, 2: 바위, 3: 보
+         */
         Scanner sc = new Scanner(System.in);
         int num1 = sc.nextInt();
         int num2 = sc.nextInt();
 
-        if(num1 > num2) System.out.println("A");
-        else if(num1 < num2) System.out.println("B");
-        else System.out.println("C");
+        if (num1 == 1) {
+            if(num2 == 3) System.out.println("A");
+            else System.out.println("B");
+        } else if (num1 == 2) {
+            if(num2 == 1) System.out.println("A");
+            else System.out.println("B");
+        } else {
+            if(num2 == 2) System.out.println("A");
+            else System.out.println("B");
+        }
 
     }
 
